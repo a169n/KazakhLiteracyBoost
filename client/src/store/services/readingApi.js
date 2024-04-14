9137ae253786e79c97747b8b36c4bb455f084879
samatbelentbay <belentbais03@mail.ru>
2024-04-14 10:17:04 +0500

    delete axios

--- client/src/store/services/readingApi.js
+++ client/src/store/services/readingApi.js
@@ -1,27 +1,5 @@
-import axiosInstance from '@/axiosInstance';
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const axiosBaseQuery = ({ baseUrl } = { baseUrl: '' }) =>
-    async ({ url, method, data, params, headers }) => {
-        try {
-            const result = await axiosInstance({
-                url: baseUrl + url,
-                method,
-                data,
-                params,
-                headers,
-            })
-            return { data: result.data }
-        } catch (axiosError) {
-            const err = axiosError
-            return {
-                error: {
-                    status: err.response?.status,
-                    data: err.response?.data || err.message,
-                },
-            }
-        }
-    }
 
 export const readingApi = createApi({
     reducerPath: 'readingApi',
